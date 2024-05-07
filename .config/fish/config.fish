1771310c95cb80ab0bee30b22a52362c7752f55c
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-07 17:06:59 +0530

    chore: added tmux shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,4 +15,3 @@ zoxide init fish | source # https://github.com/ajeetdsouza/zoxide
 fzf --fish | source # https://github.com/junegunn/fzf
 
 set -U fish_greeting # disable fish greeting
-set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
