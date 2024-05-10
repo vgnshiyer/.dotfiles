9dece4968819f312193563bf9caa61860644be0d
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-10 09:56:31 +0530

    chore: fzf config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,6 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 starship init fish | source # https://starship.rs/
 zoxide init fish | source # https://github.com/ajeetdsouza/zoxide
-fzf --fish | source # https://github.com/junegunn/fzf
+fzf --fish | source
 
 set -U fish_greeting # disable fish greeting
-set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
-set -Ux FZF_DEFAULT_OPTS "--reverse"
