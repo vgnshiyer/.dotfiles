3fae5c1d788cc86b77571ae6e16d5efb36458a15
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-10 07:23:32 +0530

    chore: added fish function lfcd

---
+++ .config/fish/functions/lfcd.fish
@@ -0,0 +1,15 @@
+# credits: https://github.com/joshmedeski/dotfiles/blob/main/.config/fish/functions/lfcd.fish
+
+function lfcd
+    set tmp (mktemp)
+    lf -last-dir-path=$tmp $argv
+    if test -f "$tmp"
+        set dir (cat $tmp)
+        rm -f $tmp
+        if test -d "$dir"
+            if test "$dir" != (pwd)
+                cd $dir
+            end
+        end
+    end
+end
