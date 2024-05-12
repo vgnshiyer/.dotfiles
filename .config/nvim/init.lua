4dc4c675cbc0bff4a676b11303fd8cb0aeb88b6e
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-12 06:23:30 +0530

    chore: added intial nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -12,4 +17,28 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+local opts = {}
 
+require("lazy").setup(plugins, opts)
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = {"java", "lua", "rust", "javascript", "typescript", "python"},
+	highlight = { enable = true },
+	indent = { enable = true }
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
