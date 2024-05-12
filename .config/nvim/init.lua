a79dabd591e39fb4681d7b66b35265b547c8e2f8
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-12 08:09:52 +0530

    chore: nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,28 +17,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"java", "lua", "rust", "javascript", "typescript", "python"},
-	highlight = { enable = true },
-	indent = { enable = true }
-})
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+require("lazy").setup("plugins")
---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+return { 
+	"catpnuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme "catppuccin"
+	end
+}
+
