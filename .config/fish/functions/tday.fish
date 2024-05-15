35c7692620ec22d8093735360c28421cc2906804
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-15 14:45:26 +0530

    chore: added new function tday; tested function `zet`

---
+++ .config/fish/functions/tday.fish
@@ -0,0 +1,18 @@
+# function that opens today's daily note.
+function tday
+    set template_file "$HOME/Documents/second-brain/888-Templates/Daily Note Template.md"
+    set daily_notes_dir "$HOME/Documents/second-brain/2-Daily Notes/"
+    set todays_date (date "+%Y-%m-%d")
+    set day (date "+%A")
+
+    set filename "$daily_notes_dir/$todays_date-$day.md"
+
+    if test -f $filename
+        echo "Today's note already exists. Opening it."
+        nvim $filename
+        return
+    end
+
+    cp $template_file $filename
+    nvim $filename
+end
--- .config/fish/functions/zet.fish
+++ .config/fish/functions/zet.fish
@@ -1,9 +1,9 @@
 function zet
     set zettel_template_dir "$HOME/Documents/second-brain/888-Templates/zettel.md"
     set inbox_dir "$HOME/Documents/second-brain/0-inbox/"
-    set todays_date (date "+%Y-%m")
+    set todays_date (date "+%Y-%m-%d_%H-%M-%S")
 
-    set filename "$inbox_dir/$todays_date-.md"
+    set filename "$inbox_dir/$todays_date.md"
 
     cp $zettel_template_dir $filename
 
