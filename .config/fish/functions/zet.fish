fd7677fb840f61ca16c479250342fb752117f14d
Vignesh Iyer <vgnshiyer@asu.edu>
2024-05-16 17:19:47 +0530

    chore: modified zet.fish

--- .config/fish/functions/zet.fish
+++ .config/fish/functions/zet.fish
@@ -3,20 +3,21 @@ function zet
     set inbox_dir "$HOME/Documents/second-brain/0-inbox/"
     set todays_date (date "+%Y-%m-%d_%H-%M-%S")
 
-    if test -z "$1"
-        then
-        set note_name (date "+%Y-%m-%d_%H-%M-%S")
+    if test -z "$argv[1]"
+        set note_name "$todays_date"
     else
-        set note_name "$1"
+        set note_name "$argv[1]"
     end
 
     set filename "$inbox_dir/$note_name.md"
 
     if test -f $filename
-        then
         echo "File already exists"
     else
-        cp $zettel_template_dir $filename
+        echo "# Created on $todays_date" >$filename
+        echo "" >>$filename
+
+        cat $zettel_template_dir >>$filename
     end
 
     nvim $filename
