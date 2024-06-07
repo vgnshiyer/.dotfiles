5d28d62df0604a3220d59434ffe8bccb2c04e84b
Vignesh Iyer <vgnshiyer@asu.edu>
2024-06-07 08:30:22 -0700

    chore: modified zet function

--- .config/fish/functions/zet.fish
+++ .config/fish/functions/zet.fish
@@ -2,6 +2,7 @@ function zet
     sync_sb
     set zettel_template_dir "$HOME/Documents/second-brain/888-Templates/zettel.md"
     set zettel_dir "$HOME/Documents/second-brain/1-zettelkasten/"
+    set zettel_pvt_dir "$HOME/Documents/second-brain/3-zettelkasten-pvt/"
     set inbox_dir "$HOME/Documents/second-brain/0-inbox/"
     set todays_date (date "+%Y-%m-%d_%H-%M-%S")
 
@@ -13,6 +14,7 @@ function zet
 
     set filename_inbox "$inbox_dir/$note_name.md"
     set filename_zettel "$zettel_dir/$note_name.md"
+    set filename_zettel_pvt "$zettel_pvt_dir/$note_name.md"
 
     if test -f $filename_inbox
         echo "File already exists"
@@ -20,6 +22,9 @@ function zet
     else if test -f $filename_zettel
         echo "File already exists"
         code $filename_zettel
+    else if test -f $filename_zettel_pvt
+        echo "file already exists"
+        code $filename_zettel_pvt
     else
         echo "Created on $todays_date" > $filename_inbox
         cat $zettel_template_dir >> $filename_inbox
