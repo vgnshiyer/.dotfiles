8cd913f9ac4c15731d08d1abc6679f3b500cc4e7
Vignesh Iyer <vgnshiyer@asu.edu>
2024-06-09 15:10:01 -0700

    chore: modified function --> zet

--- .config/fish/functions/zet.fish
+++ .config/fish/functions/zet.fish
@@ -2,9 +2,19 @@ function zet
     sync_sb
     set zettel_template_dir "$HOME/Documents/second-brain/888-Templates/zettel.md"
     set zettel_dir "$HOME/Documents/second-brain/1-zettelkasten/"
-    set zettel_pvt_dir "$HOME/Documents/second-brain/3-zettelkasten-pvt/"
+    set zettel_pvt_dir "$HOME/Documents/second-brain/1-zettelkasten-pvt/"
     set inbox_dir "$HOME/Documents/second-brain/0-inbox/"
+    set inbox_pvt_dir "$HOME/Documents/second-brain/0-inbox-pvt/"
     set todays_date (date "+%Y-%m-%d_%H-%M-%S")
+
+    set private_mode 0
+    set note_name ""
+
+    for arg in $argv
+        if test $arg = "--pvt"
+            set private_mode 1
+        end
+    end
 
     if test -z "$argv[1]"
         set note_name "$todays_date"
@@ -13,12 +23,16 @@ function zet
     end
 
     set filename_inbox "$inbox_dir/$note_name.md"
+    set filename_inbox_pvt "$inbox_pvt_dir/$note_name.md"
     set filename_zettel "$zettel_dir/$note_name.md"
     set filename_zettel_pvt "$zettel_pvt_dir/$note_name.md"
 
     if test -f $filename_inbox
         echo "File already exists"
         code $filename_inbox
+    else if test -f $filename_inbox_pvt
+        echo "File already exists"
+        code $filename_inbox_pvt
     else if test -f $filename_zettel
         echo "File already exists"
         code $filename_zettel
@@ -26,9 +40,17 @@ function zet
         echo "file already exists"
         code $filename_zettel_pvt
     else
-        echo "Created on $todays_date" > $filename_inbox
-        cat $zettel_template_dir >> $filename_inbox
-        code $filename_inbox
+        if test $private_mode -eq 1
+            echo "Created on $todays_date" > $filename_inbox_pvt
+            cat $zettel_template_dir >> $filename_inbox_pvt
+
+            code $filename_inbox_pvt
+        else
+            echo "Created on $todays_date" > $filename_inbox
+            cat $zettel_template_dir >> $filename_inbox
+            
+            code $filename_inbox
+        end
     end
 end
 
