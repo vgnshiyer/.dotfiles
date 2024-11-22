c6f9bd559f6702f5a8c7c8a12f38ad4c245e0c2a
Vignesh Iyer <vgnshiyer@asu.edu>
2024-11-22 14:59:34 -0800

    dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,14 +31,19 @@ end
 
 ## Required for setting up mysql8.0 and python mysqlclient
 set -gx PATH /usr/local/mysql/bin $PATH
+
 export DYLD_LIBRARY_PATH="/usr/local/mysql/lib" 
 
 alias ls="eza --icons=always --color=always"
 alias cl="clear"
 alias cd="z"
 alias nv="nvim"
+alias gtree="git log --pretty=oneline --graph --decorate --all"
 
 # work specific (wont necessarily work on other machines)
 alias rvim="vim svcenomis"
 alias lgstg="ssh svcenomis@sc-ipp-apsbackend-stg-vm-001"
 alias lgstg2="ssh svcenomis@sc-ipp-apsbackendworker-stg-vm-001"
+alias lgapsp2="ssh svcmobile_promotions@mobile-aps-040"
+alias lgapsp3="ssh svcmobile_promotions@sc-ipp-apsworker-dev-vm-001"
+
